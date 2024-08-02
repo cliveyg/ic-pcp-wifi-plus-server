9eb0f0002c2e2161e2cbe3b1280b6ee6e4abf466
cliveyg <junkcat@gmail.com>
2024-08-02 19:30:58 -0300

    Added shell script 'interface' to piCorePlayer functions

--- routes.go
+++ routes.go
@@ -3,7 +3,7 @@ package main
 func (a *App) initializeRoutes() {
 
 	// endpoints
-	a.Router.HandleFunc("/status", a.getStatus).Methods("GET")
+	a.Router.HandleFunc("/status", a.getSystemStatus).Methods("GET")
 	a.Router.HandleFunc("/wifi-status", a.getWifiStatus).Methods("GET")
 	a.Router.HandleFunc("/wifi-ssid", a.getWifiSSID).Methods("GET")
 }
