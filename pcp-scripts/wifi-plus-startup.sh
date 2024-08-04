8b82570290502a4273b09b295386905af208a59e
cliveyg <junkcat@gmail.com>
2024-08-04 11:38:02 -0300

    Debug startup script so binary process runs after parent process is finished

--- pcp-scripts/wifi-plus-startup.sh
+++ pcp-scripts/wifi-plus-startup.sh
@@ -18,19 +18,20 @@ if sudo cp /mnt/UserData/industrialcool-pcp-wifi-plus/wifiplus /var/www/wifiplus
   echo "Successfully copied wifi-plus binary to webroot"
   echo "Attempting to start binary..."
 
-  nohup sudo ./wifiplus > /dev/null 2>&1 &
+  cd /var/www/ && nohup ./wifiplus > /dev/null 2>&1 &
+
   printf "Binary started successfully.\nListening on port 8020...\n"
-  #echo "Testing connection..."
-  #sleep 5
-  #rc=$(curl -s -o /dev/null -w "%{http_code}" http://icplayer.local:8020/status)
-  #if [ $rc = "200" ]; then
-  #  echo " API up and running."
-  #  exit 0
-  #else
-  #  echo "Unable to connect to API successfully."
-  #  echo "Status code is [$rc]"
-  #  echo "Exiting..."
-  #fi
+  echo "Testing connection..."
+  sleep 3
+  rc=$(curl -s -o /dev/null -w "%{http_code}" http://icplayer.local:8020/status)
+  if [ $rc = "200" ]; then
+    echo "[$rc OK] API up and running."
+    exit 0
+  else
+    echo "Unable to connect to API successfully."
+    echo "Status code is [$rc]"
+    echo "Exiting..."
+  fi
 
 else
   echo "Unable to copy binary to webroot."
