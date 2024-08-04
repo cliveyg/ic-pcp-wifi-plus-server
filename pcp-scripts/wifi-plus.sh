4d9170970160d49a3aa3999fac90021f348d5611
cliveyg <junkcat@gmail.com>
2024-08-04 13:11:41 -0300

    Test calling pcp functions

--- pcp-scripts/wifi-plus.sh
+++ pcp-scripts/wifi-plus.sh
@@ -10,17 +10,27 @@
 
 subroutine=$1
 arg1=$2
-# arg2=$3
-# arg3=$4
-# arg4=$5
+arg2=$3
+arg3=$4
+arg4=$5
 
 wp_status() {
   echo $arg1
 }
 
+wp_test() {
+  pcp_set_coloured_text
+  echo "Able to call pcp functions"
+}
+
+# ---------------------- main program ---------------------- #
+
 case $subroutine in
   wp_status)
     wp_status
+  ;;
+  wp_test)
+    wp_test
   ;;
   *)
     echo "$subroutine"
--- routes.go
+++ routes.go
@@ -5,7 +5,8 @@ import log "github.com/sirupsen/logrus"
 func (a *App) initializeRoutes() {
 
 	// endpoints
-	log.Info("In initializeRoutes")
+	log.Debug("In initializeRoutes")
+	a.Router.HandleFunc("/test", a.testTings).Methods("GET")
 	a.Router.HandleFunc("/status", a.getSystemStatus).Methods("GET")
 	a.Router.HandleFunc("/wifi-status", a.getWifiStatus).Methods("GET")
 	a.Router.HandleFunc("/wifi-ssid", a.getWifiSSID).Methods("GET")
