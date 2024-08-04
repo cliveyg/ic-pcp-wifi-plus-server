796242bca25b266b38c8f077bbbbc85051c2f356
cliveyg <junkcat@gmail.com>
2024-08-04 09:52:00 -0300

    Remove softlink

--- pcp-scripts/wifi-plus-startup.sh
+++ pcp-scripts/wifi-plus-startup.sh
@@ -5,12 +5,7 @@ echo "Starting wifi-plus-startup..."
 echo "Copying go binary and script files to web folders..."
 sudo chmod 777 /mnt/UserData/industrialcool-pcp-wifi-plus/pcp-scripts/wifi-plus.sh
 if sudo cp /mnt/UserData/industrialcool-pcp-wifi-plus/pcp-scripts/wifi-plus.sh /var/www/cgi-bin/wifi-plus.sh; then
-  if sudo ln -sf /var/www/cgi-bin/wifi-plus.sh /var/www/wifi-plus.sh; then
-    echo "Successfully copied wifi-plus shell file to cgi-bin and created softlink"
-  else
-    echo "Copied wifi-plus shell file to cgi-bin but failed to creat softlink to file"
-    exit 1
-  fi
+  echo "Successfully copied wifi-plus shell file to cgi-bin"
 else
   echo "Unable to copy shell file to cgi-bin."
   echo "Exiting..."
@@ -24,7 +19,7 @@ if sudo cp /mnt/UserData/industrialcool-pcp-wifi-plus/wifiplus /var/www/wifiplus
   echo "Attempting to start binary..."
 
   sudo /var/www/wifiplus > /dev/null 2>&1 &
-  printf "Binary started successfully.\nListening on port 8020..."
+  printf "Binary started successfully.\nListening on port 8020...\n"
   echo "Testing connection..."
   rc=$(curl -s -o /dev/null -w "%{http_code}" http://icplayer.local:8020/status)
   if [ $rc = "200" ]; then
