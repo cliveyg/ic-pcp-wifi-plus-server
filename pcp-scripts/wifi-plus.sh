dbb4cce7b7d90c77c1c77a2294cfb215aa995585
cliveyg <junkcat@gmail.com>
2024-08-04 16:01:46 -0300

    Refactoring

--- pcp-scripts/wifi-plus.sh
+++ pcp-scripts/wifi-plus.sh
@@ -10,20 +10,23 @@
 
 subroutine=$1
 arg1=$2
-arg2=$3
-arg3=$4
-arg4=$5
+#arg2=$3
+#arg3=$4
+#arg4=$5
 
 wp_picore_details() {
   pv=pcp_picore_version
   pcpv=pcp_picoreplayer_version
   sqv=pcp_squeezelite_version
   lv=pcp_linux_release
-  printf "\"picore_version\": \"%s\", \"picoreplayer_version\": \"%s\", \"squeezelite_version\": \"%s\", \"linux_release\": \"%s\"" pv pcpv sqv lv
+  printf "\"picore_version\": \"%s\", " \
+         "\"picoreplayer_version\": \"%s\", " \
+         "\"squeezelite_version\": \"%s\", " \
+         "\"linux_release\": \"%s\"" $pv $pcpv $sqv $lv
 }
 
 wp_status() {
-  return $(($arg1))
+  echo "$arg1"
 }
 
 wp_test() {
