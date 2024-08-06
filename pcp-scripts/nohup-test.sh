2be9267f722a8731c3b87e8d277c44a382f7ec06
cliveyg <junkcat@gmail.com>
2024-08-06 09:20:20 -0300

    Yarp

--- pcp-scripts/nohup-test.sh
+++ pcp-scripts/nohup-test.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-nohup "$(sleep 10; touch /mnt/UserData/wifi-plus/tfile3.txt; ll /mnt/UserData/wifi-plus/tfile3.txt)" > /www/log/wifiplus.log 2>&1 &
-nohup "$(sleep 5; touch /mnt/UserData/wifi-plus/tfile2.txt; ll /mnt/UserData/wifi-plus/tfile2.txt)" > /www/log/wifiplus.log 2>&1 &
-nohup "$(sleep 1; touch /mnt/UserData/wifi-plus/tfile1.txt; ll /mnt/UserData/wifi-plus/tfile1.txt)" > /www/log/wifiplus.log 2>&1 &
+nohup "$(sleep 10; ./wifi-plus-startup.sh)" > /dev/null 2>&1 &
+nohup "$(sleep 5; touch /mnt/UserData/wifi-plus/tfile2.txt; ll /mnt/UserData/wifi-plus/tfile2.txt)" > /dev/null 2>&1 &
+nohup "$(sleep 1; touch /mnt/UserData/wifi-plus/tfile1.txt; ll /mnt/UserData/wifi-plus/tfile1.txt)" > /dev/null 2>&1 &
 echo "{ \"beep\": \"boop\" }"
