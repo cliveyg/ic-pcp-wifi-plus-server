82d221bb23abecd5cfe50a4255aad498542fd05d
cliveyg <junkcat@gmail.com>
2024-08-06 09:25:31 -0300

    Narp?

--- pcp-scripts/nohup-test.sh
+++ pcp-scripts/nohup-test.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-nohup "$(sleep 10; ./wifi-plus-startup.sh)" > /dev/null 2>&1 &
-nohup "$(sleep 5; touch /mnt/UserData/wifi-plus/tfile2.txt; ll /mnt/UserData/wifi-plus/tfile2.txt)" > /dev/null 2>&1 &
-nohup "$(sleep 1; touch /mnt/UserData/wifi-plus/tfile1.txt; ll /mnt/UserData/wifi-plus/tfile1.txt)" > /dev/null 2>&1 &
+nohup "$(sleep 12; ./wifi-plus-startup.sh)" > /dev/null 2>&1 &
+sleep 1
+nohup "$(sleep 6; /usr/local/etc/init.d/wifi wlan0 start)" > /dev/null 2>&1 &
+sleep 1
+nohup "$(/usr/local/etc/init.d/wifi wlan0 stop)" > /dev/null 2>&1 &
 echo "{ \"beep\": \"boop\" }"
