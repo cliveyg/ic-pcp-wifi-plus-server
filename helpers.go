026e7566a4099d617cd47f88fbcec99926292c18
cliveyg <junkcat@gmail.com>
2024-08-09 09:33:50 -0300

    Refactor AP mode info

--- helpers.go
+++ helpers.go
@@ -8,40 +8,22 @@ import (
 
 func (a *App) ExecCmd(command string, args []string) (string, error) {
 
-	log.Info("Starting ExecCmd")
-
 	stdout, err := exec.Command(command, args...).Output()
 	if err != nil {
 		log.WithFields(log.Fields{"err": err}).Debug("Something went wrong")
 		return "", err
 	}
-	log.WithFields(log.Fields{"stdout": string(stdout)}).Debug("Something went right")
 	return strings.TrimSpace(string(stdout)), nil
 
 }
 
-func substr(input string, start int, length int) string {
-	asRunes := []rune(input)
-	if start >= len(asRunes) {
-		return ""
-	}
-	if start+length > len(asRunes) {
-		length = len(asRunes) - start
-	}
-	return string(asRunes[start : start+length])
-}
-
 func textToMap(sg string) map[string]string {
 
-	log.Debug("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
-	log.Debug("In textToMap")
 	output := map[string]string{}
 	for _, pair := range strings.Split(strings.TrimSpace(sg), "\n") {
-		log.Debugf("PAIR IS %s", pair)
 		kv := strings.Split(pair, "=")
 		rs := strings.ReplaceAll(kv[1], "\"", "")
 		output[kv[0]] = rs
 	}
-	log.WithFields(log.Fields{"[[[output]]]": output}).Debug()
 	return output
 }
--- pcp-scripts/wp-wifi-to-wap.sh
+++ pcp-scripts/wp-wifi-to-wap.sh
@@ -23,42 +23,46 @@ set +a
 # the backend is feature complete
 LOGGY=/var/log/wifiplus.log
 
-if [ $DBUG -eq 1 ]; then
+if [ -f $TCEMNT/tce/optional/pcp-apmode.tcz ]; then
 
-  if [ -f $LOGGY ]; then
-    echo "[wp-wifi-to-wap.sh] ------------ woop ------------------" >> $LOGGY
-    #echo "[wp-wifi-to-wap.sh] ENVs are [$(printenv)]" >> $LOGGY
-    #$(pcp_config_file)
-    #$(pcp_read_config)
-    #[ cd /var/www/cgi-bin ] &&  pcp_picore_version
-    #echo -n"[wp-wifi-to-wap.sh] PCPCFG IS [$($PCPCFG)]" >> $LOGGY
-    echo "$(pcp_picore_version)" >> $LOGGY
-    echo "$PCPCFG" >> $LOGGY
-    #echo "$(pcp_config_file)"
-    #echo "$(. $PCPCFG)"
-    #echo "$(pcp_variables)"
-    echo "$(cat /etc/httpd.conf)" >> $LOGGY
+  if [ $DBUG -eq 1 ]; then
+
+    if [ -f $LOGGY ]; then
+      echo "[wp-wifi-to-wap.sh] ------------------------------------" >> $LOGGY
+
+      #echo "[wp-wifi-to-wap.sh] ENVs are [$(printenv)]" >> $LOGGY
+      #$(pcp_config_file)
+      #$(pcp_read_config)
+      #[ cd /var/www/cgi-bin ] &&  pcp_picore_version
+      #echo -n"[wp-wifi-to-wap.sh] PCPCFG IS [$($PCPCFG)]" >> $LOGGY
+      #echo "$(pcp_picore_version)" >> $LOGGY
+      #echo "$PCPCFG" >> $LOGGY
+      #echo "$(pcp_config_file)"
+      #echo "$(. $PCPCFG)"
+      #echo "$(pcp_variables)"
+      #echo "$(cat /etc/httpd.conf)" >> $LOGGY
+    else
+      sudo touch $LOGGY
+      echo "[wp-wifi-to-wap.sh] ------------------------------" >> $LOGGY
+      echo "[wp-wifi-to-wap.sh] DBUG IS [$DBUG]" >> $LOGGY
+    fi
+
+
   else
-    sudo touch $LOGGY
-    echo "[wp-wifi-to-wap.sh] ------------------------------" >> $LOGGY
-    echo "[wp-wifi-to-wap.sh] DBUG IS [$DBUG]" >> $LOGGY
+    # turning wifi off
+    pcp_write_var_to_config WIFI "off"
+    /usr/local/etc/init.d/wifi wlan0 stop
+    pcp_wifi_unload_wifi_extns "text"
+    pcp_wifi_unload_wifi_firmware_extns "text"
+    pcp_save_to_config
+    pcp_backup "text"
+    # turning wap on
+    if [ ! -x /usr/local/etc/init.d/pcp-apmode ]; then
+      pcp-load -i pcp-apmode.tcz
+    fi
+    /usr/local/etc/init.d/pcp-apmode start
   fi
 
-
-#else
-  # turning wifi off
- # export WIFI="off"
- # /usr/local/etc/init.d/wifi wlan0 stop
-	#pcp_wifi_unload_wifi_extns "text"
-	#pcp_wifi_unload_wifi_firmware_extns "text"
-  #pcp_save_to_config
-  #pcp_backup "text"
-  # turning wap on
-	#if [ ! -x /usr/local/etc/init.d/pcp-apmode ]; then
-	#	sudo -u tc pcp-load -i pcp-apmode.tcz
-	#fi
-	#sudo /usr/local/etc/init.d/pcp-apmode start
+else
+  echo '{ "status": 500, "message": "ap mode not installed" }'
 fi
-
-#echo "{ \"beep\": \"boop\", \"yarp\": \"narp\" }"
-echo '{ "beep": "boop", "yarp": "narp" }'
