f4f7cc05fb4770666417538de2871df5756a2b43
cliveyg <junkcat@gmail.com>
2024-08-15 17:31:14 -0300

    Testy test

--- pcp-scripts/wp-wifi-switch.sh
+++ pcp-scripts/wp-wifi-switch.sh
@@ -14,8 +14,6 @@ set +a
 
 LOG=$LOGFILE
 
-. /var/www/cgi-bin/pcp-functions
-
 ssid=$1
 pass=$2
 
@@ -34,7 +32,12 @@ if [ $DBUG -eq 1 ]; then
   #sudo sed -i "s/90909090909090909090909090909/$ssid/g" /usr/local/etc/pcp/wpa_supplicant.conf
   #sudo sed -i "s/\+\+/$pass/g" /usr/local/etc/pcp/wpa_supplicant.conf
   #sudo chown root:root /usr/local/etc/pcp/wpa_supplicant.conf
-  echo $(sudo -u tc pcp_backup "text") >> $LOG
+
+  # backup stuff
+  echo -n "[wp-wifi-switch.sh] backup status: " >> $LOG
+  echo $(wp_backup) >> $LOG
+
+
   #sudo wpa_cli -i wlan0 reconfigure
   sleep 3
   echo '{ "status": 200, "message": "have loggy" }'
@@ -55,5 +58,28 @@ else
   #./wifi-plus-startup.sh
 
 fi
+
+wp_backup() {
+  # basically a copy of pcp_backup() without pcp bits
+
+  # delete any previous backup_done file
+  [ -e /tmp/backup_done ] && sudo rm -f /tmp/backup_done
+
+  # do a backup - filetool.sh backs up files in .filetool.lst
+  sudo filetool.sh -b
+  sync > /dev/null 2>&1
+
+  # if backup_status file exists and is non-zero in size, then an error has occurred
+  if [ -s /tmp/backup_status ]; then
+    cat /tmp/backup_status
+  fi
+
+  # if backup_done exists, then the backup was successful
+  if [ -f /tmp/backup_done ]; then
+    return 0
+  else
+    return 1
+  fi
+}
 
 #echo "{ \"beep\": \"boop\" }"
